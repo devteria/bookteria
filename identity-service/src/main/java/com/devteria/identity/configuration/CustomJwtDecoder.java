93c19efb656e903031757a28885a2b59a4768358
devteria <devteriadotcom@gmail.com>
2024-05-12 10:37:04 +0700

    clean code identity service

--- identity-service/src/main/java/com/devteria/identity/configuration/CustomJwtDecoder.java
+++ identity-service/src/main/java/com/devteria/identity/configuration/CustomJwtDecoder.java
@@ -3,28 +3,13 @@ package com.devteria.identity.configuration;
 import java.text.ParseException;
 
 import com.nimbusds.jwt.SignedJWT;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.stereotype.Component;
-
-import com.devteria.identity.service.AuthenticationService;
 
 @Component
 public class CustomJwtDecoder implements JwtDecoder {
-    @Value("${jwt.signerKey}")
-    private String signerKey;
-
-    private final AuthenticationService authenticationService;
-
-    private NimbusJwtDecoder nimbusJwtDecoder = null;
-
-    public CustomJwtDecoder(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
-
     @Override
     public Jwt decode(String token) throws JwtException {
         try {
