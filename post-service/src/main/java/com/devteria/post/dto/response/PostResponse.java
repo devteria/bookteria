43a5a87f1902c3454146a01e68f86af55e34e797
devteria <devteriadotcom@gmail.com>
2024-08-22 22:31:08 +0700

    implement map/dictionary-based strategy

--- post-service/src/main/java/com/devteria/post/dto/response/PostResponse.java
+++ post-service/src/main/java/com/devteria/post/dto/response/PostResponse.java
@@ -18,6 +18,7 @@ public class PostResponse {
     String id;
     String content;
     String userId;
+    String created;
     Instant createdDate;
     Instant modifiedDate;
 }
--- post-service/src/main/java/com/devteria/post/service/PostService.java
+++ post-service/src/main/java/com/devteria/post/service/PostService.java
@@ -23,6 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class PostService {
+    DateTimeFormatter dateTimeFormatter;
     PostRepository postRepository;
     PostMapper postMapper;
 
@@ -48,12 +49,18 @@ public class PostService {
         Pageable pageable = PageRequest.of(page - 1, size, sort);
         var pageData = postRepository.findAllByUserId(userId, pageable);
 
+        var postList = pageData.getContent().stream().map(post -> {
+            var postResponse = postMapper.toPostResponse(post);
+            postResponse.setCreated(dateTimeFormatter.format(post.getCreatedDate()));
+            return postResponse;
+        }).toList();
+
         return PageResponse.<PostResponse>builder()
                 .currentPage(page)
                 .pageSize(pageData.getSize())
                 .totalPages(pageData.getTotalPages())
                 .totalElements(pageData.getTotalElements())
-                .data(pageData.getContent().stream().map(postMapper::toPostResponse).toList())
+                .data(postList)
                 .build();
     }
 }
