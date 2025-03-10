6d0cc92306a40f2eaa5a6adfa93b13c314420a71
Devteria <devteriadotcom@gmail.com>
2025-03-10 20:23:36 +0700

    update config

--- file-service/src/main/java/com/devteria/file/configuration/SecurityConfig.java
+++ file-service/src/main/java/com/devteria/file/configuration/SecurityConfig.java
@@ -16,7 +16,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private static final String[] PUBLIC_ENDPOINTS = {};
+    private static final String[] PUBLIC_ENDPOINTS = {
+            "/media/upload"
+    };
 
     private final CustomJwtDecoder customJwtDecoder;
 
