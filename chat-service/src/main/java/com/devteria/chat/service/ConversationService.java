cac8278258b6894c425bb112d65f1585ad741900
Devteria <devteriadotcom@gmail.com>
2025-06-19 21:33:15 +0700

    implement conversation

--- chat-service/src/main/java/com/devteria/chat/service/ConversationService.java
+++ chat-service/src/main/java/com/devteria/chat/service/ConversationService.java
@@ -3,6 +3,9 @@ package com.devteria.chat.service;
 import com.devteria.chat.dto.request.ConversationRequest;
 import com.devteria.chat.dto.response.ConversationResponse;
 import com.devteria.chat.entity.Conversation;
+import com.devteria.chat.entity.ParticipantInfo;
+import com.devteria.chat.exception.AppException;
+import com.devteria.chat.exception.ErrorCode;
 import com.devteria.chat.mapper.ConversationMapper;
 import com.devteria.chat.repository.ConversationRepository;
 import com.devteria.chat.repository.httpclient.ProfileClient;
@@ -13,7 +16,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.StringJoiner;
 
 @Slf4j
@@ -27,16 +33,70 @@ public class ConversationService {
     ConversationMapper conversationMapper;
 
     public List<ConversationResponse> myConversations() {
-        return null;
+        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+        List<Conversation> conversations = conversationRepository.findAllByParticipantIdsContains(userId);
+
+        return conversations.stream().map(this::toConversationResponse).toList();
     }
 
     public ConversationResponse create(ConversationRequest request) {
-        return null;
+        // Fetch user infos
+        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+        var userInfoResponse = profileClient.getProfile(userId);
+        var participantInfoResponse = profileClient.getProfile(
+                request.getParticipantIds().getFirst());
+
+        if (Objects.isNull(userInfoResponse) || Objects.isNull(participantInfoResponse)) {
+            throw new AppException(ErrorCode.UNCATEGORIZED_EXCEPTION);
+        }
+
+        var userInfo = userInfoResponse.getResult();
+        var participantInfo = participantInfoResponse.getResult();
+
+        List<String> userIds = new ArrayList<>();
+        userIds.add(userId);
+        userIds.add(participantInfo.getUserId());
+
+        var sortedIds = userIds.stream().sorted().toList();
+        String userIdHash = generateParticipantHash(sortedIds);
+
+        List<ParticipantInfo> participantInfos = List.of(
+            ParticipantInfo.builder()
+                    .userId(userInfo.getUserId())
+                    .username(userInfo.getUsername())
+                    .firstName(userInfo.getFirstName())
+                    .lastName(userInfo.getLastName())
+                    .avatar(userInfo.getAvatar())
+                  .build(),
+                ParticipantInfo.builder()
+                        .userId(participantInfo.getUserId())
+                        .username(participantInfo.getUsername())
+                        .firstName(participantInfo.getFirstName())
+                        .lastName(participantInfo.getLastName())
+                        .avatar(participantInfo.getAvatar())
+                        .build()
+        );
+
+        // Build conversation info
+        Conversation conversation = Conversation.builder()
+                .type(request.getType())
+                .participantsHash(userIdHash)
+                .createdDate(Instant.now())
+                .modifiedDate(Instant.now())
+                .participants(participantInfos)
+                .build();
+
+        conversation = conversationRepository.save(conversation);
+
+        return toConversationResponse(conversation);
     }
 
     private String generateParticipantHash(List<String> ids) {
         StringJoiner stringJoiner = new StringJoiner("_");
         ids.forEach(stringJoiner::add);
+
+        // SHA 256
+
         return stringJoiner.toString();
     }
 
